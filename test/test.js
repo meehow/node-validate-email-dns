1f925a43f6d91e173b7bde1a3cf09e9c6f5f9a0c
Michał Adamski <michal@ert.pl>
2015-06-23 15:05:09 +0200

    tests with co-mocha

--- test/test.js
+++ test/test.js
@@ -1,37 +1,30 @@
 'use strict';
 
-var co = require('co');
+var assert = require('assert');
+require('co-mocha');
+
 var validate = require('..');
 
 var randomDomain = Math.random().toString(35).substr(2, 60) + '.com';
 
 describe('Email validation', function() {
-
-  it('succeeds on domain with MX record', function() {
-    return co(function * () {
-      return yield validate('hostmaster@github.com');
-    });
+  it('succeeds on domain with MX record', function * () {
+    assert(yield validate('hostmaster@github.com'));
   });
 
-  it('succeeds on domain without MX record, but with A record', function() {
-    return co(function * () {
-      return yield validate('hostmaster@example.com');
-    });
+  it('succeeds on domain without MX record, but with A record', function * () {
+    assert(yield validate('hostmaster@example.com'));
   });
 
-  it('fails on non-existing domain', function() {
-    return co(function * () {
-      return yield validate('postmaster@' + randomDomain);
-    }).then(function() {
-      throw new Error('wtf');
-    }).catch(function() {});
+  it('fails on gmail address with less than 6 characters username', function * () {
+    assert.equal(yield validate('asd+asd@googlemail.com'), false);
   });
 
-    it('fails on wrong address', function() {
-    return co(function * () {
-      return yield validate('postmaster@doesnt_exist.com');
-    }).then(function() {
-      throw new Error('wtf');
-    }).catch(function() {});
+  it('fails on non-existing domain', function * () {
+    assert.equal(yield validate('postmaster@' + randomDomain), false);
+  });
+
+  it('fails on wrong address', function * () {
+    assert.equal(yield validate('postmaster@doesnt_exist.com'), false);
   });
 });
